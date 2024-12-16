9638bef3552a0501e60fbea46ad0ca9c65a1ba3a
Mattias Lasersköld <mattias.l.sk@gmail.com>
2024-12-16 14:22:56 +0100

    Print some tokens

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
 #include <filesystem>
 #include <iostream>
 #include <istream>
+#include <ranges>
+#include <string>
 
+#include "log.h"
 #include "token.h"
 #include "tokenizer.h"
 
-int main(int argc, char *argv[]) {
-    std::istream &stream = std::cin;
+struct Settings {
+    std::filesystem::path path;
 
-    auto file = Tokenizer{stream, "stdin"};
+    Settings(int argc, char *argv[]) {
+        auto args = std::vector<std::string>{argv + 1, argv + argc};
+
+        for (auto i : std::ranges::iota_view{0uz, args.size()}) {
+            auto arg = args.at(i);
+
+            path = arg;
+        }
+    }
+};
+
+int main(int argc, char *argv[]) {
+    const auto settings = Settings{argc, argv};
+
+    auto file = [&] {
+        if (settings.path.empty()) {
+            return Tokenizer(std::cin, "stdin");
+        }
+        else {
+            return Tokenizer{settings.path};
+        }
+    }();
 
     for (Token token; (file.current(TokenType::Any).type != TokenType::Eof);) {
-        std::cout << token << "\n";
+        // vlog(file.pop(TokenType::Any).text);
+        std::cout << file.pop(TokenType::Any).text << std::endl;
     }
+
+    std::cout << std::endl;
 
     return 0;
 }
